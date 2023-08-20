26b6795bfbb7fd15f8c42bdc0ee796ce6198d5a9
nataliya <nataliya.s.pavlova@gmail.com>
2023-08-20 22:03:33 +0300

    hometask3 add schema registery

--- Accounting/src/core/queue/models.py
+++ Accounting/src/core/queue/models.py
@@ -1,9 +1,20 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
+from uuid import uuid4
+import datetime
+
+
+class EventData(BaseModel):
+    public_id: str
 
 
 class Event(BaseModel):
-    name: str
-    public_id: str | None = None
+    model_config = ConfigDict(title='Event', arbitrary_types_allowed=True)
+    event_id: str = uuid4()
+    event_version: int = 1
+    event_name: str
+    event_time: str = datetime.datetime.now().strftime('%Y-%m-%d %H:%I:%S')
+    producer: str = 'accounting'
+    data: EventData
 
 
 class BEvent(Event):
